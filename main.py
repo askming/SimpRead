07609ade83e8aa7eb3ae40f3f50b50f87661de25
askming <askming@gmail.com>
2022-02-20 22:49:37 -0800

    update python script

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ SOURCE = './Saved_Reading'
 def read_list_files(sourcepath, md_name ="README.md"):
 
     source_dir = os.path.join(sourcepath, '*.md')
-    filepaths = (glob.glob(source_dir))
+    filepaths_temp = (glob.glob(source_dir))
+    filepaths = [p.replce(" ", "%20") for p in filepaths_temp]
     filenames = [p.split('/')[-1].split('.')[0] for p in filepaths]
 
     with open(md_name, "w") as f:
