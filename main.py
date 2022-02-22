b333debb2b6b2a4b1b06f77e676aae6036a03b06
askming <askming@gmail.com>
2022-02-22 09:48:11 -0800

    group by year

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
-import os
+import os, pathlib
 import glob
-from datetime import date
+import datetime
 
 SOURCE = './Saved_Reading'
 
-def read_list_files(sourcepath, md_name ="README.md"):
-
+def read_list_files(sourcepath, md_name ="./README.md"):
+    os.chdir(sourcepath)
     source_dir = os.path.join(sourcepath, '*.md')
-    filepaths_temp = glob.glob(source_dir)
-    filepaths_temp.sort()
-    filepaths = [p.replace(" ", "%20") for p in filepaths_temp]
-    filenames = [p.split('/')[-1].split('.')[0] for p in filepaths_temp]
+    filepaths = glob.glob(source_dir)
+    filepaths.sort(key=os.path.getctime) # sort file by created date
 
     with open(md_name, "w") as f:
+        current_year = ''
         f.write(f"# Saved readings from SimpRead\n\n")
-        f.write(f"_last updated on {str(date.today())}; total {len(filenames)} articles_\n\n")   
+        f.write(f"_last updated on {str(datetime.date.today())}; total {len(filepaths)} articles_\n\n")   
         for i in range(len(filepaths)):  
-            f.write(f"- [{filenames[i]}]({filepaths[i]})\n\n")
+            filepath_i = filepaths[i].replace(" ", "%20")
+            filename = filepaths[i].split('/')[-1].split('.')[0]
+            fname = pathlib.Path(filepaths[i].split('/')[-1])
+            created_date = datetime.datetime.fromtimestamp(fname.stat().st_ctime, tz=datetime.tzinfo.tzname("US/Pacific")).date()
+
+            if created_date.year != current_year:
+                f.write(f"## {created_date.year}\n\n")
+                current_year = created_date.year
+            f.write(f"- [{filename}]({filepath_i}), _added on {created_date}_\n\n")
 
 if __name__ == "__main__":
     read_list_files(sourcepath=SOURCE)
